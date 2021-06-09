0d00a27a873eec75abe8a21d3ab9bc77725b520d
Henrique Leite <henriqueleite616@gmail.com>
2021-06-09 17:07:19 -0300

    Upadate ESLint Config

--- jest.config.js
+++ jest.config.js
@@ -3,20 +3,22 @@ module.exports = {
 	rootDir: "src",
 	testRegex: ".*\\.spec\\.ts$",
 	transform: {
-		"^.+\\.(tsx|ts|jsx|js)$": "ts-jest",
+		"^.+\\.(t|j)s$": "ts-jest",
 	},
 	collectCoverageFrom: ["**/*.ts"],
-	//setupFiles: ["./v1/tests/setup.ts"],
+	// SetupFiles: ["./v1/tests/setup.ts"],
 	coverageDirectory: "../coverage",
 	testEnvironment: "node",
 	moduleDirectories: ["node_modules", "src"],
 	resetMocks: true,
-/* 	coverageThreshold: {
-		global: {
-		branches: 100,
-		functions: 100,
-		lines: 100,
-		statements: 100,
-		},
-	}, */
+	/*
+	 * CoverageThreshold: {
+	 * 	global: {
+	 * 		branches: 100,
+	 * 		functions: 100,
+	 * 		lines: 100,
+	 * 		statements: 100,
+	 * 	},
+	 * },
+	 */
 };
